128fbc68ddb448587d5b75af8a0eec84dc7464b6
Maria Pirv <mariapirv@192-168-0-102.rdsnet.ro>
2021-10-18 19:33:02 +0300

    lab 4

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def Eliminare_nrPrime (lst):
     '''
     elimina numerele prime din lista
     :param lst: lista de nr intregi
-    :return: o lista fara numere prime
+    :return: lista fara numere prime
     '''
     new_lst = []
     for x in lst:
@@ -37,17 +37,45 @@ def test_Eliminare_nrPrime():
     assert Eliminare_nrPrime([100,4,5,78]) == [100,4,78]
     assert Eliminare_nrPrime([11,13,7]) == []
 
+def MediaAritmetica_mai_mare_ca_nr (lst,n):
+    '''
+    verifica daca media aritmetica a elem din lista este mai mare decat un numar n dat
+    :param lst: lista de nr intregi
+    :param n: nr intreg
+    :return: True daca media aritmetica este mai mare decat un numar dat, sau False in caz contrar
+    '''
+    suma = 0
+    for x in lst:
+        suma = suma + x
+    media = suma // len(lst)
+    if media > n:
+        return True
+    else:
+        return False
+
+def test_MediaAritmetica_mai_mare_ca_nr():
+    assert MediaAritmetica_mai_mare_ca_nr([10, -3, 25, -1, 3, 25, 18],10) is True
+    assert MediaAritmetica_mai_mare_ca_nr([1,2,3,10],20) is False
+
 def main ():
     test_Eliminare_nrPrime()
+    test_MediaAritmetica_mai_mare_ca_nr()
     lst = []
     while True:
         print("1. Citire lista")
         print ("2. Afisarea listei dupa eliminarea numerelor prime din lista")
+        print ("3. Să se afișeze dacă media aritmetică a numerelor este mai mare decât un număr n dat.")
         optiune = input("Dati optiunea: ")
         if optiune == "1":
             lst = CitireLista()
         elif optiune == "2":
             print (Eliminare_nrPrime(lst))
+        elif optiune =="3":
+            n = int(input("Dati un numar cu care se va compara media aritmetica: "))
+            if MediaAritmetica_mai_mare_ca_nr(lst,n):
+                print ("Da")
+            else:
+                print ("Nu")
         elif optiune == "x":
             break
         else:
